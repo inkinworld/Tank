c7799e68d8e4d60c307571cba4d2566f59403bbf
inkinworld <inkinworld@live.com>
2016-03-08 23:04:46 +0800

    更新地砖碰撞判断以及损毁绘图

--- js/tile.js
+++ js/tile.js
@@ -23,7 +23,51 @@ function Tile(x,y,rota,direction,frame){
 		height: Style.tank,
 	}
 
+	this.coll = {
+		L1: {
+			state: 1,
+			list: [1,1,1,1] 
+		},
+		L2: {
+			state: 1,
+			list: [1,1,1,1] 
+		},
+		R1: {
+			state: 1,
+			list: [1,1,1,1] 
+		},
+		R2: {
+			state: 1,
+			list: [1,1,1,1] 
+		}
+	}
+
 	this.draw = function(ctx){
 		this.frame[this.state.frame].draw(ctx,this.state.x,this.state.y,Style.title,Style.title,this.state.rota);
+		this.drawDestory(ctx);
+	}
+
+	// 损毁的地砖处理
+	this.drawDestory =function(ctx){
+		var that = this;
+		var quenue = ['L1','L2','R1','R2'];
+		quenue.forEach(function(ele,index){ 
+			var item = that.coll[ele];
+			if(!item.state){
+				// console.log(ele);
+				var x = (index%2)? that.state.x : that.state.x - Style.tank/2;
+				var y = (index < 2)? that.state.y - Style.tank/2: that.state.y; 
+				console.log(x);
+				item.list.forEach(function(ele,index){
+					if(!ele){
+						// console.log(index);
+						var drawx = (index%2)? x + Style.tank/4 : x;
+						var drawy = ((index < 2)? 0 : Style.tank/4) +y;
+						// ctx.fillStyle = 'red';
+						ctx.fillRect(drawx,drawy,16,16);
+					}
+				})
+			}
+		})
 	}
 }
