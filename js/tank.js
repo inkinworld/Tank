cae9d2c190e6f4de1dfd0d812bb22126e603e6db
inkinworld <inkinworld@live.com>
2016-03-11 10:17:57 +0800

    坦克添加开火功能

--- js/tank.js
+++ js/tank.js
@@ -38,6 +38,23 @@ function MyTank(x,y,rota,direction,frame){
 
 	this.keyBoard = function(){
 		var that = this;
+		//发射子弹
+		function fire(){
+			switch(that.state.direction){
+				case 0:
+					Bullets.list.push(new Bullet(that.state.x,that.state.y - Style.tank/2,0,0,0));
+					break;
+				case 1:
+					Bullets.list.push(new Bullet(that.state.x + Style.tank/2,that.state.y,90,1,0));
+					break;
+				case 2:
+					Bullets.list.push(new Bullet(that.state.x,that.state.y + Style.tank/2,180,2,0));
+					break;
+				case 3:
+					Bullets.list.push(new Bullet(that.state.x - Style.tank/2,that.state.y,270,3,0));
+					break;
+			}
+		}
 		Action.keyBind('down','W',function(){
 			that.state.direction = 0;
 			that.moveState.w = 1;
@@ -78,6 +95,8 @@ function MyTank(x,y,rota,direction,frame){
 		Action.keyBind('up','D',function(){
 			that.moveState.d = 0;
 		})
+
+		Action.keyBind('down','J',fire)
 	}
 
 	this.update = function(frames,tileList){
@@ -88,7 +107,7 @@ function MyTank(x,y,rota,direction,frame){
 		moveState.isMove = moveState.w || moveState.a || moveState.s || moveState.d;
 		if(moveState.isMove){
 			state.frame = Math.floor(frames/4)%2;
-			switch(Tanks.myTank.state.direction){
+			switch(that.state.direction){
 				case 0:
 					state.y -= Style.tankSpeed;
 					correct(0);
@@ -108,21 +127,44 @@ function MyTank(x,y,rota,direction,frame){
 			}
 
 			tileList.forEach(function(ele){
-				if( Collision.isColl(that,ele).isColl){
-					switch(Tanks.myTank.state.direction){
-						case 0:
-							state.y = ele.state.y + (ele.graph.height);
-							break;
-						case 1:
-							state.x = ele.state.x - (ele.graph.height);
-							break;
-						case 2:
-							state.y = ele.state.y - (ele.graph.height);
-							break;
-						case 3:
-							state.x = ele.state.x + (ele.graph.height);
-							break;
-					}
+				var collResult = Collision.isColl(that,ele);
+				var childList = []; 
+				if( collResult.isColl ){
+					childList = ele.createChildColl();
+					if(!childList.length) return;
+					childList.forEach(function(ele){
+						var collResult = Collision.isColl(that,ele);
+						if(collResult.isColl){
+							switch(that.state.direction){
+								case 0:
+									state.y = ele.state.y + (ele.graph.height/2) + (Style.tank/2);
+									break;
+								case 1:
+									state.x = ele.state.x - (ele.graph.height/2) - (Style.tank/2);
+									break;
+								case 2:
+									state.y = ele.state.y - (ele.graph.height/2) - (Style.tank/2);
+									break;
+								case 3:
+									state.x = ele.state.x + (ele.graph.height/2) + (Style.tank/2);
+									break;
+							}
+						}
+					})
+					// switch(Tanks.myTank.state.direction){
+					// 	case 0:
+					// 		state.y = ele.state.y + (ele.graph.height);
+					// 		break;
+					// 	case 1:
+					// 		state.x = ele.state.x - (ele.graph.height);
+					// 		break;
+					// 	case 2:
+					// 		state.y = ele.state.y - (ele.graph.height);
+					// 		break;
+					// 	case 3:
+					// 		state.x = ele.state.x + (ele.graph.height);
+					// 		break;
+					// }
 					return;
 				}
 			})
@@ -143,7 +185,6 @@ function MyTank(x,y,rota,direction,frame){
 					break;
 			}
 		}
-
 
 	}
 
