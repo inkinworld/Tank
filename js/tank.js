6cfb25cda07e487dd5447587cef3ed1744c06995
inkinworld <inkinworld@live.com>
2016-03-12 22:50:53 +0800

    添加mytank类

--- js/tank.js
+++ js/tank.js
@@ -1,18 +1,18 @@
 var Tanks = {};
 // 坦克素材图片，在load模块加载；
 var tankImg;
-
-function MyTank(x,y,rota,direction,frame){
+Tank.prototype = new Sprite(Style.tank ,Style.tank ,Style.tank ,Style.tank);
+function Tank(){
 	this.state = {
-		x : x,
-		y : y,
-		rota : rota,
+		x : 0,
+		y : 0,
+		rota : 0,
 		// 0 --> up
 		// 1 --> right
 		// 2 --> down
 		// 3 --> left 
-		direction : direction,
-		frame : frame
+		direction : 0,
+		frame : 0
 	}
 
 	this.moveState = {
@@ -23,17 +23,112 @@ function MyTank(x,y,rota,direction,frame){
 		isMove: false
 	}
 
-	this.graph = {
-		width: Style.tank,
-		height: Style.tank
+	this.update = function(frames,tileList){
+		// 引用自身
+		var that = this;
+		var state = Tanks.myTank.state,
+			moveState = Tanks.myTank.moveState;
+		moveState.isMove = moveState.w || moveState.a || moveState.s || moveState.d;
+		if(moveState.isMove){
+			state.frame = Math.floor(frames/4)%2;
+			switch(that.state.direction){
+				case 0:
+					state.y -= Style.tankSpeed;
+					correct(0);
+					break;
+				case 1:
+					state.x += Style.tankSpeed;
+					correct(1);
+					break;
+				case 2:
+					state.y += Style.tankSpeed;
+					correct(0);
+					break;
+				case 3:
+					state.x -= Style.tankSpeed;
+					correct(1);
+					break;
+			}
+
+			tileList.forEach(function(ele){
+				var collResult = Collision.isColl(that,ele);
+				var childList = []; 
+				if( collResult.isColl ){
+					childList = ele.createChildColl();
+					if(!childList.length) return;
+					childList.forEach(function(ele){
+						var collResult = Collision.isColl(that,ele);
+						if(collResult.isColl){
+							switch(that.state.direction){
+								case 0:
+									state.y = ele.state.y + (ele.volume.height/2) + (Style.tank/2);
+									break;
+								case 1:
+									state.x = ele.state.x - (ele.volume.height/2) - (Style.tank/2);
+									break;
+								case 2:
+									state.y = ele.state.y - (ele.volume.height/2) - (Style.tank/2);
+									break;
+								case 3:
+									state.x = ele.state.x + (ele.volume.height/2) + (Style.tank/2);
+									break;
+							}
+						}
+					})
+					// switch(Tanks.myTank.state.direction){
+					// 	case 0:
+					// 		state.y = ele.state.y + (ele.volume.height);
+					// 		break;
+					// 	case 1:
+					// 		state.x = ele.state.x - (ele.volume.height);
+					// 		break;
+					// 	case 2:
+					// 		state.y = ele.state.y - (ele.volume.height);
+					// 		break;
+					// 	case 3:
+					// 		state.x = ele.state.x + (ele.volume.height);
+					// 		break;
+					// }
+					return;
+				}
+			})
+			// 修正超出画布
+			if(state.x < Style.tank/2) state.x = Style.tank/2;
+			if(state.x > Style.canvas-32) state.x = Style.canvas-32;
+			if(state.y < Style.tank/2) state.y = Style.tank/2;
+			if(state.y > Style.canvas-32) state.y = Style.canvas-32;
+		} 
+		// 修正运动
+		function correct(type){
+			switch(type){
+				case 0:
+					state.x = Math.round(state.x / (Style.tank / 2)) * (Style.tank/2);
+					break;
+				case 1:
+					state.y = Math.round(state.y / (Style.tank / 2)) * (Style.tank/2);
+					break;
+			}
+		}
+
 	}
 
-	this.frame = [];
-	this.frame.push(new Sprite(tankImg,0,0,32,32));
-	this.frame.push(new Sprite(tankImg,448,0,32,32));
+}
 
-	this.draw = function(ctx){
-		this.frame[this.state.frame].draw(ctx,this.state.x,this.state.y,Style.tank,Style.tank,this.state.rota);
+MyTank.prototype = new Tank();
+function MyTank(x,y,rota,direction,frame){
+	this.addFrame(tankImg,0,0,32,32);
+	this.addFrame(tankImg,448,0,32,32);
+
+	this.state = {
+		x : x,
+		y : y,
+		rota : rota,
+		// 0 --> up
+		// 1 --> right
+		// 2 --> down
+		// 3 --> left 
+		direction : direction,
+		frame : frame
 	}
 
 	this.keyBoard = function(){
@@ -98,94 +193,4 @@ function MyTank(x,y,rota,direction,frame){
 
 		Action.keyBind('down','J',fire)
 	}
-
-	this.update = function(frames,tileList){
-		// 引用自身
-		var that = this;
-		var state = Tanks.myTank.state,
-			moveState = Tanks.myTank.moveState;
-		moveState.isMove = moveState.w || moveState.a || moveState.s || moveState.d;
-		if(moveState.isMove){
-			state.frame = Math.floor(frames/4)%2;
-			switch(that.state.direction){
-				case 0:
-					state.y -= Style.tankSpeed;
-					correct(0);
-					break;
-				case 1:
-					state.x += Style.tankSpeed;
-					correct(1);
-					break;
-				case 2:
-					state.y += Style.tankSpeed;
-					correct(0);
-					break;
-				case 3:
-					state.x -= Style.tankSpeed;
-					correct(1);
-					break;
-			}
-
-			tileList.forEach(function(ele){
-				var collResult = Collision.isColl(that,ele);
-				var childList = []; 
-				if( collResult.isColl ){
-					childList = ele.createChildColl();
-					if(!childList.length) return;
-					childList.forEach(function(ele){
-						var collResult = Collision.isColl(that,ele);
-						if(collResult.isColl){
-							switch(that.state.direction){
-								case 0:
-									state.y = ele.state.y + (ele.graph.height/2) + (Style.tank/2);
-									break;
-								case 1:
-									state.x = ele.state.x - (ele.graph.height/2) - (Style.tank/2);
-									break;
-								case 2:
-									state.y = ele.state.y - (ele.graph.height/2) - (Style.tank/2);
-									break;
-								case 3:
-									state.x = ele.state.x + (ele.graph.height/2) + (Style.tank/2);
-									break;
-							}
-						}
-					})
-					// switch(Tanks.myTank.state.direction){
-					// 	case 0:
-					// 		state.y = ele.state.y + (ele.graph.height);
-					// 		break;
-					// 	case 1:
-					// 		state.x = ele.state.x - (ele.graph.height);
-					// 		break;
-					// 	case 2:
-					// 		state.y = ele.state.y - (ele.graph.height);
-					// 		break;
-					// 	case 3:
-					// 		state.x = ele.state.x + (ele.graph.height);
-					// 		break;
-					// }
-					return;
-				}
-			})
-			// 修正超出画布
-			if(state.x < Style.tank/2) state.x = Style.tank/2;
-			if(state.x > Style.canvas-32) state.x = Style.canvas-32;
-			if(state.y < Style.tank/2) state.y = Style.tank/2;
-			if(state.y > Style.canvas-32) state.y = Style.canvas-32;
-		} 
-		// 修正运动
-		function correct(type){
-			switch(type){
-				case 0:
-					state.x = Math.round(state.x / (Style.tank / 2)) * (Style.tank/2);
-					break;
-				case 1:
-					state.y = Math.round(state.y / (Style.tank / 2)) * (Style.tank/2);
-					break;
-			}
-		}
-
-	}
-
 }
