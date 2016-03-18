0e31a9e704fd4e15b1694e23baf899659308d09a
inkinworld <inkinworld@live.com>
2016-03-18 16:33:24 +0800

    子弹对不同类型的地砖进行不同的处理

--- js/bullet.js
+++ js/bullet.js
@@ -13,7 +13,7 @@ var miscImg;
 
 Bullet.prototype = new Sprite(Style.bullet ,Style.bullet ,Style.bullet/2 ,Style.bullet/2);
 
-function Bullet(x,y,rota,direction,frame){
+function Bullet(x,y,rota,direction,frame,type){
 	this.state = {
 		x : x,
 		y : y,
@@ -35,7 +35,7 @@ function Bullet(x,y,rota,direction,frame){
 	}
 
 	this.exist = 1;
-
+	this.type = type? type:0; 
 	this.remove =function(ctx){
 		ctx.fillRect(this.state.x -8,this.state.y -8,16,16);
 	}	
@@ -101,13 +101,15 @@ function Bullet(x,y,rota,direction,frame){
 			}
 		}
 
-		collTestList.forEach(function bulletCollTest(ele){
-			if(!ele) return;
+		collTestList.forEach(function bulletCollTest(tile){
+			if(!tile) return;
+			var isBoom = false;
+			var boomType = 0;
 			var childList = []; 
-			childList = ele.createChildColl();
+			var tileType = tile.type;
+			childList = tile.createChildColl();
 			if(!childList.length) return;
 			childList.forEach(function bulletCollTest1(ele){
-				var isBoom = false;
 				var collResult = Collision.isColl(that,ele);
 				if(collResult.isColl){
 					var item = ele.tile;
@@ -120,10 +122,12 @@ function Bullet(x,y,rota,direction,frame){
 									//射中小砖块的左下角区域，[小方块][2]
 									if(item.list[2]){
 										//小砖块的左下角( [小方块][2] )存在，摧毁小砖块的左下角和右下角
-										item.list[2] = 0;
-										item.list[3] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[2] = 0;
+										// item.list[3] = 0;
+										var result = handleDestory(tileType,item.list,[2,3])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 									//否则无影响
 								}else{
@@ -131,10 +135,12 @@ function Bullet(x,y,rota,direction,frame){
 									// console.log( collResult.position.dy );
 									if(item.list[0]){
 										//小砖块的左上角( [小方块][0] )存在，摧毁小砖块的左上角和右上角
-										item.list[0] = 0;
-										item.list[1] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[0] = 0;
+										// item.list[1] = 0;
+										var result = handleDestory(tileType,item.list,[0,1])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 									//否则无影响
 								}
@@ -144,20 +150,24 @@ function Bullet(x,y,rota,direction,frame){
 									//射中小砖块的右下角区域，[小方块][3]
 									if(item.list[3]){
 										//小砖块的右下角( [小方块][2] )存在，摧毁小砖块的左下角和右下角
-										item.list[2] = 0;
-										item.list[3] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[2] = 0;
+										// item.list[3] = 0;
+										var result = handleDestory(tileType,item.list,[2,3])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 									//否则无影响
 								}else{
 									//射中小砖块的右上角区域, [小方块][1]
 									if(item.list[1]){
 										//小砖块的左上角( [小方块][1] )存在，摧毁小砖块的左上角和右上角
-										item.list[0] = 0;
-										item.list[1] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[0] = 0;
+										// item.list[1] = 0;
+										var result = handleDestory(tileType,item.list,[0,1])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 									//否则无影响
 								}
@@ -169,33 +179,41 @@ function Bullet(x,y,rota,direction,frame){
 								//射中小砖块的上侧区域
 								if( collResult.position.dx <= -(Style.bullet/2) ){
 									if(item.list[0]){
-										item.list[0] = 0;
-										item.list[2] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[0] = 0;
+										// item.list[2] = 0;
+										var result = handleDestory(tileType,item.list,[0,2])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}else{
 									if(item.list[1]){
-										item.list[1] = 0;
-										item.list[3] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[1] = 0;
+										// item.list[3] = 0;
+										var result = handleDestory(tileType,item.list,[1,3])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}
 							}else{
 								if( collResult.position.dx <= -(Style.bullet/2) ){
 									if(item.list[2]){
-										item.list[0] = 0;
-										item.list[2] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[0] = 0;
+										// item.list[2] = 0;
+										var result = handleDestory(tileType,item.list,[0,2])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}else{
 									if(item.list[3]){
-										item.list[1] = 0;
-										item.list[3] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[1] = 0;
+										// item.list[3] = 0;
+										var result = handleDestory(tileType,item.list,[1,3])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}
 							}
@@ -206,33 +224,41 @@ function Bullet(x,y,rota,direction,frame){
 								//射中小砖块的左侧区域
 								if( collResult.position.dy <= -(Style.bullet/2) ){
 									if(item.list[0]){
-										item.list[0] = 0;
-										item.list[1] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[0] = 0;
+										// item.list[1] = 0;
+										var result = handleDestory(tileType,item.list,[0,1])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}else{
 									if(item.list[2]){
-										item.list[2] = 0;
-										item.list[3] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[2] = 0;
+										// item.list[3] = 0;
+										var result = handleDestory(tileType,item.list,[2,3])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}
 							}else{
 								if( collResult.position.dy <= -(Style.bullet/2) ){
 									if(item.list[1]){
-										item.list[1] = 0;
-										item.list[0] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[1] = 0;
+										// item.list[0] = 0;
+										var result = handleDestory(tileType,item.list,[1,0])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}else{
 									if(item.list[3]){
-										item.list[3] = 0;
-										item.list[2] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[3] = 0;
+										// item.list[2] = 0;
+										var result = handleDestory(tileType,item.list,[2,3])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}
 							}
@@ -243,45 +269,63 @@ function Bullet(x,y,rota,direction,frame){
 								//射中小砖块的上侧区域
 								if( collResult.position.dx >= (Style.bullet/2) ){
 									if(item.list[1]){
-										item.list[1] = 0;
-										item.list[3] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[1] = 0;
+										// item.list[3] = 0;
+										var result = handleDestory(tileType,item.list,[1,3])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}else{
 									if(item.list[0]){
-										item.list[0] = 0;
-										item.list[2] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[0] = 0;
+										// item.list[2] = 0;
+										var result = handleDestory(tileType,item.list,[0,2])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}
 							}else{
 								if( collResult.position.dx >= (Style.bullet/2) ){
 									if(item.list[3]){
-										item.list[1] = 0;
-										item.list[3] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[1] = 0;
+										// item.list[3] = 0;
+										var result = handleDestory(tileType,item.list,[1,3])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}else{
 									if(item.list[2]){
-										item.list[0] = 0;
-										item.list[2] = 0;
-										that.exist = 0;
-										isBoom = true;
+										// item.list[0] = 0;
+										// item.list[2] = 0;
+										var result = handleDestory(tileType,item.list,[0,2])
+										isBoom = result.isBoom;
+										boomType = result.boomType;
+								
 									}
 								}
 							}
 							item.complete = 0;
 							break;
 					}
-					if(isBoom) Booms.list.push(new Boom(that.state.x,that.state.y,0,0,0,0))	
 				}
 			})
+		
+			if(isBoom){
+				switch(boomType){
+					case 0:
+						Booms.list.push(new Boom(that.state.x,that.state.y,0,0,0,0));
+						break;
+					case 1:
+						Booms.list.push(new Boom(tile.state.x,tile.state.y,0,0,0,1));
+
+				}
+			}
 			// 与子弹发生作用的地砖进行状态更新与损毁绘制
-			ele.update();	
-			ele.drawDestory(ctx);
+			tile.update();	
+			tile.drawDestory(ctx);
 			return;
 		})
 
@@ -313,8 +357,47 @@ function Bullet(x,y,rota,direction,frame){
 			this.exist = 0;
 
 		function addCollTest(col,row){
-			if(0<=row && row <13 &&  0 <= col && col <13) collTestList.push(map[col][row]);
+			if(0<=row && row <13 &&  0 <= col && col <13) collTestList.push(map.mapArray[col][row]);
 		}
-	}
+
+		function handleDestory(tileType,item,destoryList){
+			switch(that.type){
+				case 0:
+					return bullet0Handle(tileType,item,destoryList);
+				case 1:
+					return bullet1Handle(tileType,item,destoryList);
+			}
+		}
+
+		function bullet0Handle(tileType,item,destoryList){
+			switch(tileType){
+				case 0:
+					destoryList.forEach(function(num){
+						item[num] = 0;
+					})
+					that.exist = 0;
+					return {
+						isBoom: true,
+						boomType: 0
+					};
+				case 1:
+					that.exist = 0;
+					return {
+						isBoom: true,
+						boomType: 0
+					};
+				case 3:
+					return {
+						isBoom: false
+					};
+				case 5:
+					that.exist = 0;
+					return {
+						isBoom:true,
+						boomType: 1
+					};
+			}
+		}
+	}// end of this.update;
 
 }
