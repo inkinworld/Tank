6b213a63ba47caa7c2e49a5ba21abd1fef63e8df
inkinworld <inkinworld@live.com>
2016-03-18 10:36:12 +0800

    子弹碰撞检测逻辑修改为二维数组模式，修改子弹数组清除函数

--- js/bullet.js
+++ js/bullet.js
@@ -1,12 +1,11 @@
 var Bullets = {};
-Bullets.list = [];
+Bullets.teamList = [[],[]];
 function BulletsClear(){
-	var newList = [];
-	Bullets.list.forEach(function(ele){
-		if(ele.exist)
-			newList.push(ele);
+	Bullets.teamList.forEach(function(list){
+		list.forEach(function(ele,index){
+			if(!ele.exist) list.splice(index,1);
+		})
 	})
-	Bullets.list = newList;
 }
 
 // 子弹图片load模块
@@ -41,7 +40,7 @@ function Bullet(x,y,rota,direction,frame){
 		ctx.fillRect(this.state.x -8,this.state.y -8,16,16);
 	}	
 
-	this.update = function(tileList,ctx){
+	this.update = function(ctx,teamNum){
 		var collTestList = [];
 		var that =this;
 		var state = this.state;
@@ -277,7 +276,7 @@ function Bullet(x,y,rota,direction,frame){
 							item.complete = 0;
 							break;
 					}
-					if(isBoom) Booms.list.push(new Boom(that.state.x,that.state.y,0,0,0))	
+					if(isBoom) Booms.list.push(new Boom(that.state.x,that.state.y,0,0,0,0))	
 				}
 			})
 			// 与子弹发生作用的地砖进行状态更新与损毁绘制
@@ -286,222 +285,36 @@ function Bullet(x,y,rota,direction,frame){
 			return;
 		})
 
+		Tanks.teamList.forEach(function(team,index){
+			if(index === teamNum) return;
+			team.forEach(function(tank){
+				var collResult = Collision.isColl(that,tank);
+				if(collResult.isColl){ 
+					Booms.list.push(new Boom(tank.state.x,tank.state.y,0,0,0,1));
+					that.exist = 0;
+					tank.exist = 0;
+				}				
+			})
+		})
+
+		Bullets.teamList.forEach(function(team,index){
+			if(index === teamNum) return;	
+			team.forEach(function(bullet){
+				var collResult = Collision.isColl(that,bullet);
+				if(collResult.isColl){ 
+					that.exist = 0;
+					bullet.exist = 0;
+				}				
+			})
+		})
+		
+
 		if((state.x < Style.bullet/2) || (state.x > Style.canvas-Style.bullet/2) || (state.y < Style.bullet/2) || (state.y > Style.canvas-Style.bullet/2) ) 
 			this.exist = 0;
 
 		function addCollTest(col,row){
 			if(0<=row && row <13 &&  0 <= col && col <13) collTestList.push(map[col][row]);
 		}
-	}
-
-
-	this.updateBackup = function(tileList,ctx){
-		var that =this;
-		var state = this.state;
-		switch(state.direction){
-			case 0:
-				state.y -= Style.bulletSpeed;
-				break;
-			case 1:
-				state.x += Style.bulletSpeed;
-				break;
-			case 2:
-				state.y += Style.bulletSpeed;
-				break;
-			case 3:
-				state.x -= Style.bulletSpeed;
-				break;
-		}
-
-		tileList.forEach(function bulletCollTest(ele){
-			var collResult = Collision.isColl(that,ele);
-			var childList = []; 
-			if( collResult.isColl ){
-				childList = ele.createChildColl();
-				if(!childList.length) return;
-				childList.forEach(function bulletCollTest1(ele){
-					var isBoom = false;
-					var collResult = Collision.isColl(that,ele);
-					if(collResult.isColl){
-						var item = ele.tile;
-						switch(state.direction){
-							case 0:
-								if(collResult.position.dx < 0 ){
-									//射中小砖块的左侧区域
-									if( collResult.position.dy >= (Style.bullet/2) ){
-										// console.log( collResult.position.dy );
-										//射中小砖块的左下角区域，[小方块][2]
-										if(item.list[2]){
-											//小砖块的左下角( [小方块][2] )存在，摧毁小砖块的左下角和右下角
-											item.list[2] = 0;
-											item.list[3] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-										//否则无影响
-									}else{
-										//射中小砖块的左上角区域, [小方块][0]
-										// console.log( collResult.position.dy );
-										if(item.list[0]){
-											//小砖块的左上角( [小方块][0] )存在，摧毁小砖块的左上角和右上角
-											item.list[0] = 0;
-											item.list[1] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-										//否则无影响
-									}
-								}else{
-									//射中小砖块的右侧
-									if( collResult.position.dy >= (Style.bullet/2) ){
-										//射中小砖块的右下角区域，[小方块][3]
-										if(item.list[3]){
-											//小砖块的右下角( [小方块][2] )存在，摧毁小砖块的左下角和右下角
-											item.list[2] = 0;
-											item.list[3] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-										//否则无影响
-									}else{
-										//射中小砖块的右上角区域, [小方块][1]
-										if(item.list[1]){
-											//小砖块的左上角( [小方块][1] )存在，摧毁小砖块的左上角和右上角
-											item.list[0] = 0;
-											item.list[1] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-										//否则无影响
-									}
-								}
-								item.complete = 0;
-								break;
-							case 1:
-								if(collResult.position.dy < 0 ){
-									//射中小砖块的上侧区域
-									if( collResult.position.dx <= -(Style.bullet/2) ){
-										if(item.list[0]){
-											item.list[0] = 0;
-											item.list[2] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}else{
-										if(item.list[1]){
-											item.list[1] = 0;
-											item.list[3] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}
-								}else{
-									if( collResult.position.dx <= -(Style.bullet/2) ){
-										if(item.list[2]){
-											item.list[0] = 0;
-											item.list[2] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}else{
-										if(item.list[3]){
-											item.list[1] = 0;
-											item.list[3] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}
-								}
-								item.complete = 0;
-								break;
-							case 2:
-								if(collResult.position.dx < 0 ){
-									//射中小砖块的左侧区域
-									if( collResult.position.dy <= -(Style.bullet/2) ){
-										if(item.list[0]){
-											item.list[0] = 0;
-											item.list[1] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}else{
-										if(item.list[2]){
-											item.list[2] = 0;
-											item.list[3] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}
-								}else{
-									if( collResult.position.dy <= -(Style.bullet/2) ){
-										if(item.list[1]){
-											item.list[1] = 0;
-											item.list[0] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}else{
-										if(item.list[3]){
-											item.list[3] = 0;
-											item.list[2] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}
-								}
-								item.complete = 0;
-								break;
-							case 3:
-								if(collResult.position.dy < 0 ){
-									//射中小砖块的上侧区域
-									if( collResult.position.dx >= (Style.bullet/2) ){
-										if(item.list[1]){
-											item.list[1] = 0;
-											item.list[3] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}else{
-										if(item.list[0]){
-											item.list[0] = 0;
-											item.list[2] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}
-								}else{
-									if( collResult.position.dx >= (Style.bullet/2) ){
-										if(item.list[3]){
-											item.list[1] = 0;
-											item.list[3] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}else{
-										if(item.list[2]){
-											item.list[0] = 0;
-											item.list[2] = 0;
-											that.exist = 0;
-											isBoom = true;
-										}
-									}
-								}
-								item.complete = 0;
-								break;
-						}
-						if(isBoom) Booms.list.push(new Boom(that.state.x,that.state.y,0,0,0))	
-					}
-				})
-				// 与子弹发生作用的地砖进行状态更新与损毁绘制
-				ele.update();	
-				ele.drawDestory(ctx);
-				return;
-			}
-
-		})
-
-		if((state.x < Style.bullet/2) || (state.x > Style.canvas-Style.bullet/2) || (state.y < Style.bullet/2) || (state.y > Style.canvas-Style.bullet/2) ) 
-			this.exist = 0;
 	}
 
 }
