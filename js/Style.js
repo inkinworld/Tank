e773d3359dffac6043bc2c1e3305f4a29c7db18a
inkinworld <inkinworld@live.com>
2016-03-18 16:36:09 +0800

    修改爆炸的大小

--- js/Style.js
+++ js/Style.js
@@ -5,4 +5,11 @@ Style.tile = Style.tank;
 Style.canvas = Style.tank*13;
 Style.bullet = Style.tank/4;
 Style.bulletSpeed = Style.tankSpeed * 2;
-Style.boom = Style.tank * 1.6;
+Style.boom = Style.tank * 1.8;
+
+
+var Type = {};
+Type.tank_hero = 0;
+Type.tank_army = 1;
+Type.tile_tile = 0;
+Type.tile_iron = 1; 
