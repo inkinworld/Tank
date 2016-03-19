69a2cb2f521637bcc546b2678237bb46801a85fd
inkinworld <inkinworld@live.com>
2016-03-19 20:31:06 +0800

    重构，使用Game模块进行渲染

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
 function main(){
 	var ctx,
-	iframes;
+	    iframes;
 	map = readMapData(mapData);
 	//添加帧图片
-	Tank.prototype.addFrame(tankImg,0,0,32,32);
-	Tank.prototype.addFrame(tankImg,448,0,32,32);
-	Tank.prototype.addFrame(tankImg,128,0,32,32);
-	Tank.prototype.addFrame(tankImg,576,0,32,32);
-	Tile.prototype.addFrame(tileImg,1120,0,32,32);
-	Tile.prototype.addFrame(tileImg,640,0,32,32);
-	Tile.prototype.addFrame(tileImg,0,0,32,32);
-	Tile.prototype.addFrame(tileImg,96,0,32,32);
-	Tile.prototype.addFrame(tileImg,128,0,32,32);
-	Tile.prototype.addFrame(tileImg,32,0,32,32);
-	Tile.prototype.addFrame(tileImg,64,0,32,32);
-	Bullet.prototype.addFrame(miscImg,0,0,8,8);	
+	Game.Tank.addFrames();
+	Game.Tile.addFrames();
+	Game.Bullet.addFrames();
+	Game.Boom.addFrames();
 
+	//获得个图层
 	ctx = document.getElementById('canvas').getContext('2d');
 	boomCtx = document.getElementById('boomLayer').getContext('2d');
 	grassCtx = document.getElementById('grassLyer').getContext('2d');
 	tankCtx = document.getElementById('tankLayer').getContext('2d');
+	
 	Tanks.myTank = new MyTank(96,800,0,0,0,0);
 	Tanks.myTank.keyBoard();
 	Tanks.teamList[0].push(Tanks.myTank);
@@ -30,81 +24,38 @@ function main(){
 	Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
 
 
-	var iframes = 0;
+	iframes = 0;
 
 	// 每帧绘制流程
 	// 绘制顺序  背景 -》 地砖 -》 坦克 -》 子弹 -》 爆炸
 	// 状态更新顺序： 地砖与坦克状态依赖子弹与之的碰撞，故子弹更新早于地砖和坦克
 
 	//绘制背景
-	ctx.fillRect(0,0,Style.canvas,Style.canvas);
-	map.mapArray.forEach(function(col,index){
-		col.forEach(function(tile,i){
-			if(!tile) return;	
-			// console.log(index,i)
-			// console.log(tile);
-			tile.update();
-			tile.draw(ctx);
-			tile.drawDestory(ctx);
-		})
-	})
-	map.grassArray.forEach(function(tile){
-		tile.update();
-		tile.draw(grassCtx);
-		tile.drawDestory(grassCtx);		
-	})
+	Game.Tile.renderBg(ctx);
+	//绘制草地
+	Game.Tile.renderGrass(grassCtx);
 
 	function draw(){
 		iframes = (iframes + 1)%60;
 
 		//改变河流状态
-		map.riverArray.forEach(function(river){
-			river.state.frame = Math.floor(iframes/20)%2  + 3;
-			river.draw(ctx);
-		})
+		Game.Tile.renderRiver(ctx,iframes);
 		//清除坦克图层
 		tankCtx.clearRect(0,0,832,832);
-		//子弹状态更新
-		Bullets.teamList.forEach(function(team,index){
-			team.forEach(function bulletupdate(ele){
-				// ele.remove(ctx);
-				ele.update(ctx,index);
-			})
-			//无效子弹剔除子弹数l组
-			BulletsClear();
-		})
-		
-		// 子弹绘制
-		Bullets.teamList.forEach(function(team){
-			team.forEach(function bulletupdate(ele){
-				ele.draw(tankCtx);
-			})
-		})
 
-		TanksClear()
-		Tanks.teamList.forEach(function(team){
-			team.forEach(function tankupdate(ele){
-				// ele.remove(tankCtx);
-				ele.update(iframes);
-				ele.draw(tankCtx);			
-			})
-		})
+		//子弹渲染
+		Game.Bullet.render(ctx,tankCtx);
 
-		// 爆炸绘制；
-		//清除爆炸图层
-		boomCtx.clearRect(0,0,832,832);
-		Booms.list.forEach(function(ele){
-			ele.update();
-		})
-		BoomClear();
-		Booms.list.forEach(function(ele){
-			ele.draw(boomCtx);
-		})
+		//坦克渲染
+		Game.Tank.render(tankCtx,iframes);
 
-		window.requestAnimationFrame(draw);
+		//爆炸渲染
+		Game.Boom.render(boomCtx);
+
+		// if(App.state.play)
+			window.requestAnimationFrame(draw);
 	}
-
-	window.requestAnimationFrame(draw);
+		window.requestAnimationFrame(draw);
 }
 
 function createAitank(){
