21b89e2ab04d4b3b249129a36bceb77a0ab6deb6
inkinworld <inkinworld@live.com>
2016-03-21 16:29:02 +0800

    添加缩略图尺寸

--- js/Style.js
+++ js/Style.js
@@ -3,9 +3,11 @@ Style.tank = 64;
 Style.tankSpeed = Style.tank/16;
 Style.tile = Style.tank;
 Style.canvas = Style.tank*13;
+Style.canvasWidth = Style.tank * 15;
 Style.bullet = Style.tank/4;
 Style.bulletSpeed = Style.tankSpeed * 2;
 Style.boom = Style.tank * 1.8;
+Style.enemyThumb = Style.tank/2;
 
 
 var Type = {};
