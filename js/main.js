5088450c747ec8e7177f3e086eb8b3c891d29623
inkinworld <inkinworld@live.com>
2016-03-21 16:31:33 +0800

    通过App模块重写

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
 function main(){
 
 	//获得个图层
-	var tileCtx = document.getElementById('canvas').getContext('2d');
-	var boomCtx = document.getElementById('boomLayer').getContext('2d');
-	var grassCtx = document.getElementById('grassLyer').getContext('2d');
-	var tankCtx = document.getElementById('tankLayer').getContext('2d');
+	var tile = document.getElementById('canvas')
+	var boom = document.getElementById('boomLayer')
+	var grass = document.getElementById('grassLyer')
+	var tank = document.getElementById('tankLayer')
+	var buffer = document.getElementById('buffer')
 	
-	Tanks.myTank = new MyTank(96,800,0,0,0,0);
-	Tanks.myTank.keyBoard();
-	Tanks.teamList[0].push(Tanks.myTank);
-	// Tanks.teamList[1].push(new AiTank(96,32,0,0,2,1));
-	// Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
-	// Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
-	// Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
-	// Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
-
-	App.prepare(tileCtx,boomCtx,grassCtx,tankCtx);
-	App.begin();
+	App.prepare(tile,boom,grass,tank,buffer);
+	window.requestAnimationFrame(App.main);	
 	// App.beignMission();
 	// App.renderGame();
 
