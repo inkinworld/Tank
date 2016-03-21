2b738cbaf7e8a625af97a6d9a6ea3c1f473c4efc
inkinworld <inkinworld@live.com>
2016-03-21 16:32:33 +0800

    增加游戏逻辑模块

--- js/gameRender.js
+++ js/gameRender.js
@@ -3,6 +3,35 @@ Game.Tank = {};
 Game.Tile = {};
 Game.Bullet = {};
 Game.Boom = {};
+Game.Thumbnail = {};
+Game.Logic = {};
+
+Game.Thumbnail.render =function(ctx){
+	ctx.save();
+	ctx.fillStyle ='gray';
+	ctx.fillRect(Style.canvas,0,2*Style.canvas,Style.canvas);
+	ctx.restore();
+	var dy = 0;
+	var enemyLength = Game.Logic.enemyServive;
+	for(var i = 0;i < enemyLength;i++){
+		if(!(i%2)){
+			ctx.drawImage(miscImg,0,16,14,16,Style.tile*13.4,Style.tile+dy,Style.enemyThumb,Style.enemyThumb);
+		}else{
+			ctx.drawImage(miscImg,0,16,14,16,Style.tile*14,Style.tile+dy,Style.enemyThumb,Style.enemyThumb);
+			dy += Style.tile * 0.6;
+		}
+	}
+	ctx.drawImage(miscImg,16,16,14,16,Style.tile*13.4,Style.tile*8,Style.enemyThumb,Style.enemyThumb);
+	ctx.drawImage(miscImg,128,0,64,64,Style.tile*13.4,Style.tile*9,Style.tile,Style.tile);
+	ctx.save();
+	ctx.font = "32px prstartk";
+    ctx.fillStyle = "black";
+	ctx.fillText('1P',Style.tile*13.4,Style.tile*7.8);	    
+	ctx.fillText(Game.Logic.heroHp,Style.tile*14,Style.tile*8.5);
+	ctx.fillText(1,Style.tile*14,Style.tile*10);
+	ctx.restore();
+
+}
 
 Game.Tank.addFrames = function(){
 	Tank.prototype.addFrame(tankImg,0,0,32,32);
@@ -22,16 +51,6 @@ Game.Tank.render = function(ctx,frame){
 	})
 }
 
-Game.Tank.createHero = function(){
-	Tanks.myTank = new MyTank(96,800,0,0,0,0);
-	Tanks.myTank.keyBoard();
-	Tanks.teamList[0].push(Tanks.myTank);
-}
-
-Game.Tank.createEnemy = function(){
-	Tanks.teamList[1].push(new AiTank(96,32,0,0,2,1));
-}
-
 Game.Tile.addFrames = function(){
 	Tile.prototype.addFrame(tileImg,1120,0,32,32);
 	Tile.prototype.addFrame(tileImg,640,0,32,32);
@@ -45,6 +64,10 @@ Game.Tile.addFrames = function(){
 
 Game.Tile.renderBg = function(ctx){
 	ctx.fillRect(0,0,Style.canvas,Style.canvas);
+	App.ctx.tile.save();
+	App.ctx.tile.fillStyle = 'gray';
+	App.ctx.tile.fillRect(Style.canvas,0,2*Style.canvas,Style.canvas);
+	App.ctx.tile.restore();
 	map.mapArray.forEach(function(col,index){
 		col.forEach(function(tile,i){
 			if(!tile) return;	
@@ -116,3 +139,96 @@ Game.Boom.render = function(ctx){
 		ele.draw(ctx);
 	})	
 }
+
+Game.Logic.currentMission = 0;
+Game.Logic.enemyNum = 20;
+Game.Logic.enemyServive = 20;
+Game.Logic.heroHp = 3;
+Game.Logic.enemyTime = 0;
+Game.Logic.isHero1Servive = 0;
+Game.Logic.isGameOver = 0;
+Game.Logic.isBegin = 0;
+Game.Logic.isSuccess = 0;
+Game.Logic.isPause = 0;
+Game.Logic.heroCounter = newCounter(1);
+Game.Logic.enemyCounter = newCounter(0,200);
+
+Game.Logic.initGame = function(){
+	Game.Logic.currentMission = 0;
+	Game.Logic.enemyNum = 20;
+	Game.Logic.enemyServive = 20;
+	Game.Logic.heroHp = 3;
+	Game.Logic.enemyTime = 0;
+	Game.Logic.isHero1Servive = 0;
+	Game.Logic.isGameOver = 0;
+	Game.Logic.isBegin = 0;
+	Game.Logic.isSuccess = 0;
+	Game.Logic.isPause = 0;
+	Game.Logic.heroCounter = newCounter(1);
+	Game.Logic.enemyCounter = newCounter(0,200);
+
+	//重置坦克数组，子弹数组，爆炸数组；
+	Tanks.teamList = [[],[]];
+	Bullets.teamList = [[],[]];
+	Booms.list = [];
+}
+
+Game.Logic.gameStatae = function(){
+	var logic = Game.Logic;
+	if(logic.heroHp === 0){
+		logic.isGameOver = 1;
+		return;
+	}
+	if(logic.enemyServive === 0) logic.isSuccess = 1;
+}
+
+Game.Logic.createHero = function(){
+	var hero;
+	var logic = Game.Logic;
+	if(logic.isGameOver) return;
+	if(logic.isHero1Servive) return;
+	if(Game.Logic.heroCounter.n()<50){
+		Game.Logic.heroCounter.add();
+	}else{
+		//解除WASDJ的按键绑定
+		Action.keyBind.deBind('W');
+		Action.keyBind.deBind('A');
+		Action.keyBind.deBind('S');
+		Action.keyBind.deBind('D');
+		Action.keyBind.deBind('J');
+		logic.isHero1Servive = 1;
+		hero = new MyTank(320,800,0,0,0,0);
+		hero.keyBoard();
+		Tanks.teamList[0].push(hero);
+		Game.Logic.heroCounter.clear();
+	}
+}
+
+Game.Logic.createEnemy = function(){
+	if(Game.Logic.enemyCounter.n() === 100){
+		if(Game.Logic.enemyNum === 0) return;
+		var logic = Game.Logic;
+		var team = Tanks.teamList[1];
+		var n = 5 - team.length; 
+		if(n === 0) return;
+		if( (n >= 3) && (Game.Logic.enemyNum >= 3) ){
+			Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
+			Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
+			Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
+			Game.Logic.enemyNum -= 3;
+		}
+		if(n === 2 && (Game.Logic.enemyNum >= 2)){
+			Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
+			Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));
+			Game.Logic.enemyNum -= 2;
+		}
+		if(n === 1 && (Game.Logic.enemyNum >= 1)){
+			Tanks.teamList[1].push(new AiTank(0,0,0,0,2,1));	
+			Game.Logic.enemyNum -= 1;	
+		}
+	}else{
+		Game.Logic.enemyCounter.add();
+	}
+
+}
+
